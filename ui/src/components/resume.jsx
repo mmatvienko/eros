60f187c05e19975560bc3cc64022d1ae10cb8797
Marc Matvienko <m.matvienko@utexas.edu>
2019-06-15 19:04:06 -0500

    1st rough draft done

--- ui/src/components/resume.jsx
+++ ui/src/components/resume.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Education from "./education";
 import Job from "./job";
+import Academia from "./academia";
+import Project from "./project";
+import "./css/resume.css";
 
 class Resume extends Component {
   constructor() {
@@ -11,6 +14,18 @@ class Resume extends Component {
       error: null
     };
     console.log("Resume - Constructor");
+  }
+
+  drawLine() {
+    return (
+      <div className="row">
+        <div className="col-md-3" />
+        <div className="col-md-6">
+          <hr />
+        </div>
+        <div className="col-md-3" />
+      </div>
+    );
   }
 
   componentDidMount() {
@@ -38,12 +53,42 @@ class Resume extends Component {
     console.log("Resume - Rendered");
     return (
       <React.Fragment>
+        <div className="row">
+          <div className="col-md-3" />
+          <div className="col-md-6">
+            <div className="title-name">Marc Matvienko</div>
+            <div className="subtitle-name">
+              Life is either a daring adventure or nothing at all.
+            </div>
+          </div>
+          <div className="col-md-3" />
+        </div>
+
+        {this.drawLine()}
+
         {this.state.resume &&
           this.state.resume.education.map(school => (
             <Education key={school.id} school={school} />
           ))}
+
+        {this.drawLine()}
+
         {this.state.resume &&
           this.state.resume.jobs.map(job => <Job key={job.id} job={job} />)}
+
+        {this.drawLine()}
+
+        {this.state.resume &&
+          this.state.resume.academia.map(academ => (
+            <Academia key={academ.id} academ={academ} />
+          ))}
+
+        {this.drawLine()}
+
+        {this.state.resume &&
+          this.state.resume.projects.map(project => (
+            <Project key={project.id} project={project} />
+          ))}
       </React.Fragment>
     );
   }
